e798b6cdca22aa15b7d68029b1fa70002dd95b8f
Carlos Beneto Turner <carlosbenetoturner@gmail.com>
2025-11-15 14:04:06 +1000

    Added types and tidied up state

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,38 @@ import {useState} from 'react'
 
 function App() {
 
-  const [standard, setStandard] = useState(getRandomStandard())
-  const [leader, setLeader] = useState(getRandomWake())
-  const [follower, setFollower] = useState(getRandomWake())
-  
-  const separation = checkSeparation(standard, leader, follower)
+  const generateScenario = () => {
+    let scenario = {
+      standard: getRandomStandard(),
+      leader: getRandomWake(),
+      follower: getRandomWake(),
+      leaderType: '',
+      followerType: ''
+    }
+      scenario.separation = checkSeparation(scenario.standard, scenario.leader, scenario.follower)
+      scenario.leaderType = getRandomType(scenario.leader)
+      scenario.followerType = getRandomType(scenario.follower)
+      return scenario
+  }
+
+  const [scenario, setScenario] = useState(generateScenario())
   const [buttonState, setButtonState] = useState("Reveal")
   const reset = () => {
     if (buttonState === "Reset"){
-          setStandard(getRandomStandard())
-          setLeader(getRandomWake)
-          setFollower(getRandomWake)
-          setButtonState("Reveal")
-          return
+      setScenario(generateScenario())
+      setButtonState("Reveal")
+      return
     }
     setButtonState("Reset")
   }
-
   return (
     <>
       <h2>Wake Turbulence Standards Quiz</h2>
-      <h2>{standard.LABEL}</h2>
-      <h2><b>{leader}</b> followed by <b>{follower}</b></h2>
+      <h2>{scenario.standard.LABEL}</h2>
+      <h2><b>{scenario.leaderType}</b> followed by <b>{scenario.followerType}</b></h2>
       <button className = {buttonState === "Reset" ? "reset" : "reveal"} onClick = {reset}>{buttonState}</button>
-      <p>Minutes: {buttonState === "Reset" && separation.mins }</p>
-      <p>Distance: {buttonState === "Reset" && separation.distance}</p>
+      <p>Minutes: {buttonState === "Reset" && scenario.separation.mins }</p>
+      <p>Distance: {buttonState === "Reset" && scenario.separation.distance}</p>
          
     </>
   )
